7bc38a52c09c7f1a2ef46bf3b984efe7c8239dda
Joey Zhao <zhaoyi0113@gmail.com>
2019-04-18 15:35:10 +1000

    update layout of services

--- src/app/components/ServiceList/index.tsx
+++ src/app/components/ServiceList/index.tsx
@@ -5,27 +5,40 @@ import { withStyles } from '@material-ui/core/styles';
 import * as PropTypes from 'prop-types';
 
 
-const styles = theme => ({
+const styles = (theme:any) => ({
   root: {
     flexGrow: 1,
+    alignItems: "center",
+    backgroundColor: 'red',
+  },
+  parent: {
+    alignItems: 'center',
+    margin: 'auto',
   },
   paper: {
     height: 140,
     width: 100,
+    margin: 'auto'
   },
   control: {
     padding: theme.spacing.unit * 2,
   },
+  items: {
+  },
+  item: {
+  }
 });
+
+const itemsArray = [1,2,3,4,5,6,7,8,9];
 
 const ServiceListComponent = ({classes}: any) => {
 
   return (
-    <Grid container className={classes.root} spacing={16}>
-      <Grid item xs={12}>
-        <Grid container className={classes.demo} justify="center" spacing={16}>
-          {[0, 1, 2].map(value => (
-            <Grid key={value} item>
+    <Grid container className={classes.root} spacing={8}>
+      <Grid item className={classes.parent} xs={8}>
+        <Grid container className={classes.items} justify="center" spacing={8}>
+          {itemsArray.map(value => (
+            <Grid key={value} item className={classes.item} xs={6} md={4} xl={3}>
               <Paper className={classes.paper} />
             </Grid>
           ))}
