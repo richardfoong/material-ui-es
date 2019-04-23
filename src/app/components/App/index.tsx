5b2339ec368d1d85559c14366e1469e6250da3f8
Joey Zhao <zhaoyi0113@gmail.com>
2019-04-23 16:07:59 +1000

    update search style

--- src/app/components/App/index.tsx
+++ src/app/components/App/index.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { ServiceList } from "app-components/ServiceList";
 import { Header } from "app-components/Header";
+import { Search } from "app-components/Search";
 
 export default () => (
   <Grid container direction='column'>
     <Header />
+    <Search />
     <ServiceList />
   </Grid>
 );
--- src/app/components/Search/index.tsx
+++ src/app/components/Search/index.tsx
@@ -1,21 +1,46 @@
 import * as React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { withStyles, createStyles } from '@material-ui/core/styles';
+import { FormLabel } from '@material-ui/core';
 
-const BackgroundPanelStyles = createStyles({
+const Styles = createStyles({
   root: {
     height: '16rem',
+    background: 'radial-gradient(207.68px at 50% 18.87%, #006488 0%, #025572 100%), linear-gradient(180deg, #3E474D 0%, #292F33 100%);',
+  },
+  welcomeText: {
+    color: 'white',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '1.125rem',
+    lineHeight: '2rem',
+    textAlign: 'center',
+    margin: 'auto',
+  },
+  scanText: {
+    fontFamily: 'AP Letter',
+    fontSize: '2.5rem',
+    lineHeight: '3.5rem',
+    textAlign: 'center',
   }
 });
 
-interface Props { classes: { [className in keyof typeof BackgroundPanelStyles]: string } };
+interface Props { classes: { [className in keyof typeof Styles]: string } };
 
-const BackgroundPanelComponent = ({ classes }: Props) => {
+const BackgroundPanel = ({ classes }: Props) => {
   return (
-    <Grid container className={classes.root}>
-
+    <Grid container className={classes.root} direction='column'>
+      <FormLabel className={classes.welcomeText}>Welcome to the new POS</FormLabel>
+      <FormLabel className={classes.scanText}>Scan, Search, Sell</FormLabel>
     </Grid>
   )
 };
 
-export const BackgroundPanel = withStyles(BackgroundPanelStyles)(BackgroundPanelComponent);
+const SearchComponent = ({ classes }: Props) => {
+  return (
+    <BackgroundPanel classes={classes} />
+  )
+};
+
+export const Search = withStyles(Styles)(SearchComponent);
